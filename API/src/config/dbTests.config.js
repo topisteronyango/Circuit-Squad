cfdda9c7f4b092a3241d14899b7c6266bd281d9c
PhantomVasploit <paulvasgit99@gmail.com>
2023-04-15 09:03:14 +0300

    Project Finalization

--- API/src/config/dbTests.config.js
+++ API/src/config/dbTests.config.js
@@ -3,21 +3,20 @@ const {Sequelize} = require('sequelize');
 const logger = require('./winston.config');
 
 const sequelize = new Sequelize(
-    process.env.DATABASE_NAME, 
+    process.env.TEST_DATABASE_NAME, 
     process.env.DATABASE_USERNAME, 
     process.env.DATABASE_PASSWORD, 
     { 
-        host: process.env.DATABASE_SERVER_NAME,
-        dialect: "mssql",
-        logging: false
+        host: '127.0.0.1',
+        dialect: "mysql"
     });
 
 
 
 sequelize.authenticate()
 .then(()=>{
-    logger.info('Successfully connected to the database server');
-    console.log('Successfully connected to the database server');
+    logger.info('Successfully connected to the test database server');
+    console.log('Successfully connected to the test database server');
 })
 .catch((e)=>{
     throw e;
